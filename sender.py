cd609482f660da4020cb7334b71bc691a509fc00
akram2032 <akrammmatari203@gmail.com>
2023-05-06 11:14:30 +0100

    insert as device 1

--- sender.py
+++ sender.py
@@ -22,7 +22,7 @@ def on_publish(client, userdata, mid, properties=None):
 
 
 def main() -> None:
-    device_id = 7
+    device_id = 1
     temperature = 7
     turbidite = 3
     latitude = 3.7749
