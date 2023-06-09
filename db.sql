473df48ddff54dd727bc71ad9cdd23f0543b50dc
akram2032 <89083331+akram2032@users.noreply.github.com>
2023-06-10 00:15:50 +0100

    some changes

--- db.sql
+++ db.sql
@@ -1,35 +1,36 @@
 ---user table---
 CREATE TABLE if not exists users (
 	user_id int(4) AUTO_INCREMENT,
-    username varchar(25),
-    password varchar(25),
-    CONSTRAINT pk_users PRIMARY KEY(user_id)
+  username varchar(25),
+  password varchar(25),
+  CONSTRAINT pk_users PRIMARY KEY(user_id)
 );
 --- device table----
 create table if not exists devices (
-	device_id int(4), 
+	device_id int(4) auto_increment, 
 	country varchar(25), 
 	state varchar(25),
 	city varchar(25),
 	added_on date DEFAULT CURRENT_DATE, 
 	added_by int(4),
-  	Constraint pk_device PRIMARY KEY(device_id),
-  	Constraint fk_devices Foreign key (added_by) References users (user_id)
+  Constraint pk_device PRIMARY KEY(device_id),
+  Constraint fk_devices Foreign key (added_by) References users (user_id)
 );
 
 --- message tables----
 create table if not exists messages (
-    id int(50) not NULL auto_increment PRIMARY KEY,
-    device_id int(4) not null,
-    date date DEFAULT CURRENT_DATE,
-    time Time DEFAULT CURRENT_TIME,
-    temperature float,
-    longitude double,
-    latitude float,
-    altitude float,
-    depth float,
-    rssi int,
-    snr double,
-    turbidite float,
-    CONSTRAINT fk_msg foreign key (device_id) references devices(device_id)
+  id int(50) not NULL auto_increment,
+  device_id int(4),
+  date date DEFAULT CURRENT_DATE,
+  time Time DEFAULT CURRENT_TIME,
+  temperature float,
+  longitude double,
+  latitude float,
+  altitude float,
+  depth float,
+  rssi int,
+  snr double,
+  turbidite float,
+  CONSTRAINT pk_msg primary key (device_id),
+  CONSTRAINT fk_msg foreign key (device_id) references devices(device_id)
 );
